6934e9a878dcdca13bb704ca8a7876645f6b2cc7
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2022-02-02 08:56:55 +0700

    Add files via upload

--- blocks/generator_DFPlayerMini_Fast.js
+++ blocks/generator_DFPlayerMini_Fast.js
@@ -2,11 +2,11 @@ Blockly.JavaScript['DFPlayerMini_Fast_init'] = function(block) {
   var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
   var number_RX = block.getFieldValue('RX');
   var number_TX = block.getFieldValue('TX');
-  var code = `#EXTINC #include <DFPlayerMini_Fast.h>\n
+  var code = `#EXTINC #include <DFPlayer_Mini_Mp3.h>\n
               #include <HardwareSerial.h> #END
-#VARIABLE DFPlayerMini_Fast ${variable_instance}; \nHardwareSerial Myserial(1);#END
+#VARIABLE HardwareSerial Myserial(1);#END
 Myserial.begin(9600, SERIAL_8N1, ${number_RX}, ${number_TX});
-${variable_instance}.begin(Myserial);\n`;
+mp3_set_serial(Myserial);\n`;
   return code;
 };
 
@@ -14,19 +14,19 @@ ${variable_instance}.begin(Myserial);\n`;
 Blockly.JavaScript['DFPlayerMini_Fast_volume'] = function(block) {
   var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
   var value_volume = Blockly.JavaScript.valueToCode(block, 'volume', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = variable_instance + '.volume('+value_volume+');   \n';
+  var code = 'mp3_set_volume('+value_volume+');   \n';
   return code;
 };
 Blockly.JavaScript['DFPlayerMini_Fast_play'] = function(block) {
   var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
   var value_num = Blockly.JavaScript.valueToCode(block, 'num', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = variable_instance + '.play('+value_num+');   \n';
+  var code = 'mp3_play('+value_num+');   \n';
   return code;
 };
 Blockly.JavaScript['DFPlayerMini_Fast_loop'] = function(block) {
   var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
   var value_num = Blockly.JavaScript.valueToCode(block, 'num', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = variable_instance + '.loop('+value_num+');   \n';
+  var code = 'mp3_single_loop('+value_num+');   \n';
   return code;
 };
 Blockly.JavaScript['DFPlayerMini_Fast_playFolder'] = function(block) {
@@ -38,17 +38,17 @@ Blockly.JavaScript['DFPlayerMini_Fast_playFolder'] = function(block) {
 };
 Blockly.JavaScript['DFPlayerMini_Fast_playNext'] = function(block) {
   var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
-  var code = variable_instance + '.playNext();   \n';
+  var code = 'mp3_next();\n';
   return code;
 };
 Blockly.JavaScript['DFPlayerMini_Fast_playPrevious'] = function(block) {
   var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
-  var code = variable_instance + '.playPrevious();   \n';
+  var code = 'mp3_prev();\n';
   return code;
 };
 Blockly.JavaScript['DFPlayerMini_Fast_stop'] = function(block) {
   var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
-  var code = variable_instance + '.stop();   \n';
+  var code =  'mp3_stop();   \n';
   return code;
 };
 Blockly.JavaScript['DFPlayerMini_Fast_reset'] = function(block) {
@@ -63,7 +63,7 @@ Blockly.JavaScript['DFPlayerMini_Fast_resume'] = function(block) {
 };
 Blockly.JavaScript['DFPlayerMini_Fast_pause'] = function(block) {
   var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
-  var code = variable_instance + '.pause();   \n';
+  var code = 'mp3_pause();   \n';
   return code;
 };
 Blockly.JavaScript['DFPlayerMini_Fast_sleep'] = function(block) {
